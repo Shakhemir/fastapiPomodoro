31668d612cad68782601ae213433fc32a44338bb
Shakhemir <shakh@kaspiytelekom.ru>
2025-04-17 15:51:11 +0300

    add fixtures

---
+++ fixtures.py
@@ -0,0 +1,7 @@
+tasks = [
+    {"id": 1, "name": "task 1", "pomodoro_count": 10, "category_id": 1},
+    {"id": 2, "name": "task 2", "pomodoro_count": 10, "category_id": 2},
+    {"id": 3, "name": "task 3", "pomodoro_count": 10, "category_id": 3},
+    {"id": 4, "name": "task 4", "pomodoro_count": 10, "category_id": 4},
+]
+categories = []
